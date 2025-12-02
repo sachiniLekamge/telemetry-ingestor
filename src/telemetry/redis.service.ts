a9bc6d83d77aefad2f34194011277b8972b7f105
IT20159030 <it20159030@my.sliit.lk>
2025-12-02 16:53:41 +0530

    feat: add RedisService for managing Redis connections

---
+++ src/telemetry/redis.service.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Logger,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisService implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger(RedisService.name);
+  private client: Redis | null = null;
+
+  constructor(private readonly configService: ConfigService) {}
+
+  async onModuleInit() {}
+
+  async onModuleDestroy() {}
+}
