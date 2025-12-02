246b8158bf399fd995319ca45c75d694965eb260
IT20159030 <it20159030@my.sliit.lk>
2025-12-02 17:06:35 +0530

    feat: add RedisService and configure Mongoose and HttpModule in TelemetryModule

--- src/telemetry/telemetry.module.ts
+++ src/telemetry/telemetry.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { HttpModule } from '@nestjs/axios';
 import { TelemetryController } from './telemetry.controller';
 import { TelemetryService } from './telemetry.service';
+import { RedisService } from './redis.service';
+import { Telemetry, TelemetrySchema } from './schemas/telemetry.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Telemetry.name, schema: TelemetrySchema },
+    ]),
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [TelemetryController],
-  providers: [TelemetryService]
+  providers: [TelemetryService, RedisService],
 })
 export class TelemetryModule {}
