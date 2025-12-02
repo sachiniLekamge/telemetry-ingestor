bfe88b593d4aa77b97698ca50146a72bb7de09b4
IT20159030 <it20159030@my.sliit.lk>
2025-12-02 17:14:18 +0530

    feat: add global validation pipe to bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   await app.listen(port);
   logger.log(`Application is running on: http://localhost:${port}`);
