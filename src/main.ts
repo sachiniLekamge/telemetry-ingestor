4e5ddd9bdd01d158afb58bf8b8fea8e883a315cf
IT20159030 <it20159030@my.sliit.lk>
2025-12-02 17:13:40 +0530

    feat: configure dynamic port retrieval in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
 
-  await app.listen(3000);
-  logger.log('Application started');
+  await app.listen(port);
+  logger.log(`Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
